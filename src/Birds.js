1c4f5b86f2458e0aaafb02bc0086333e8f99015c
Leigh Halliday <leighhalliday@gmail.com>
2017-10-23 21:47:54 -0400

    Injecting store into component

--- src/Birds.js
+++ src/Birds.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import { inject, observer } from 'mobx-react';
 
+@inject('BirdStore')
+@observer
 export default class Birds extends Component {
   render() {
+    const { BirdStore } = this.props;
+
     return <div>Birds</div>;
   }
 }
