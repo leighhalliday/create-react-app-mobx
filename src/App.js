09366d84b024b611c9ca25ca883f5e0dd90a43f1
Leigh Halliday <leighhalliday@gmail.com>
2017-10-23 21:42:39 -0400

    Setting up MobX Provider

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from 'mobx-react';
+import BirdStore from './stores/BirdStore';
+import Birds from './Birds';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Provider BirdStore={BirdStore}>
+        <Birds />
+      </Provider>
     );
   }
 }
---
+++ src/Birds.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class Birds extends Component {
+  render() {
+    return <div>Birds</div>;
+  }
+}
